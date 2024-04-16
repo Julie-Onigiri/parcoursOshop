5e2485bfe7d9d8a1dab6153cce3fe67d911cb3b2
Julie-Onigiri <loptibus@gmail.com>
2024-04-16 11:24:03 +0200

    adminController

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,7 +1,16 @@
+const { User } = require("../models");
+
 const adminController = {
-    index: async (req, res) => {
-        res.render('dashboard/dashboard');
-    },
-};
+    async index(req, res) {
+        try {
+            // Je recupere la liste des utilisateur
+            const users = await User.findAll();
+            res.render('admin', { users })
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).render('500');
+        }
+    }
+}
 
 module.exports = adminController;
